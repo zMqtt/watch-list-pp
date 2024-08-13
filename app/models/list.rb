f0aa5cc8d5c7482e250d723b75af74e68d274368
matt shilling <matthewds@sky.com>
2024-08-13 15:07:08 +0100

    schema created and associations done

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,5 @@
 class List < ApplicationRecord
   belongs_to :movie
-  has_many :bookmarks
+  has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,4 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks
+  has_many :bookmarks, dependent: :restrict_with_error
+  has_many :lists, through: :bookmarks
 end
